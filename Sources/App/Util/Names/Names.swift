2fa79df08a4db11f05de5dfb6f8d980f8deef21d
Søren Gregersen <s@grgrsn.dk>
2017-01-17 22:02:17 +0100

    Using String.capitalized from Foundation

--- Sources/App/Util/Names/Names.swift
+++ Sources/App/Util/Names/Names.swift
@@ -1,16 +1,11 @@
+import Foundation
+
 class Names {
     class func getNames() -> [String] {
         preconditionFailure("This method should be overridden")
     }
 
     class func containsName(_ name: String) -> Bool {
-        return self.getNames().contains(name.uppercaseFirst)
-    }
-}
-
-extension String {
-    var uppercaseFirst: String {
-        let first = String(characters.prefix(1))
-        return first.uppercased() + String(self.lowercased().characters.dropFirst())
+        return self.getNames().contains(name.capitalized)
     }
 }
