daac9304b6fef2bf15eb3b5910988f044b4659d8
Søren Gregersen <s@grgrsn.dk>
2017-01-17 21:44:08 +0100

    Using uppercase first

--- Sources/App/Util/Names/Names.swift
+++ Sources/App/Util/Names/Names.swift
@@ -2,4 +2,15 @@ class Names {
     class func getNames() -> [String] {
         preconditionFailure("This method should be overridden")
     }
+
+    class func containsName(_ name: String) -> Bool {
+        return self.getNames().contains(name.uppercaseFirst)
+    }
+}
+
+extension String {
+    var uppercaseFirst: String {
+        let first = String(characters.prefix(1))
+        return first.uppercased() + String(characters.dropFirst())
+    }
 }
