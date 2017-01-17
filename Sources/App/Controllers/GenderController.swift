dcf67433fc1142d2ef681233d974d573f0ef4012
Søren Gregersen <s@grgrsn.dk>
2017-01-17 20:31:05 +0100

    Added GenderController

---
+++ Sources/App/Controllers/GenderController.swift
@@ -0,0 +1,8 @@
+import HTTP
+import Vapor
+
+final class GenderController {
+    func getGender(_ req: Request, _ name: String) throws -> ResponseRepresentable {
+        return "Hello, \(name)"
+    }
+}
